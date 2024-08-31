09241fe61c3c47fc1feab4509af78639593df37a
Shirish Koirala <shirishkoirala@gmail.com>
2024-08-31 22:22:06 +1000

    Initial Commit

---
+++ AutoActivityIndicator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoActivityIndicator
+//
+//  Created by Shirish Koirala on 31/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
